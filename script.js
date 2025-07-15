6765f66c2435c9577de05dc0117a9a38e95d272a
mahdi-shouib <mahdiomar402@gmail.com>
2025-07-15 14:01:11 +0300

    Add styles and moved grid styles to it

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
 const body = document.querySelector('body');
 const container = document.getElementById('container');
 
-body.style.display = 'flex';
-body.style.justifyContent = 'center';
-body.style.alignItems = 'center';
-body.style.height = '100vh';
-body.style.margin = '0';
-
 for(let i = 0; i < 16; i++){
     const row = document.createElement('div');
-    row.style.display = 'flex';
+    row.classList.add('row');
     for(let j = 0; j < 16; j++){
         const cell = document.createElement('div');
-        cell.style.width = '20px';
-        cell.style.height = '20px';
-        cell.style.border = '1px solid black';
+        cell.classList.toggle('cell');
         row.appendChild(cell);
     }
     container.appendChild(row);
