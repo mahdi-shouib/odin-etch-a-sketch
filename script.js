0c6481c4313993f798aa0da40a140901d55270e9
mahdi-shouib <mahdiomar402@gmail.com>
2025-07-15 15:40:11 +0300

    Add new grid button and better grid logic

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const body = document.querySelector('body');
 const container = document.getElementById('container');
+const newButton = document.getElementById('new');
 
-for(let i = 0; i < 16; i++){
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for(let j = 0; j < 16; j++){
-        const cell = document.createElement('div');
-        cell.classList.toggle('cell');
-        cell.addEventListener('mouseover', () => {
-            cell.classList.add('hover');
-        });
-        row.appendChild(cell);
+
+function createGrid(side) {
+    const width = container.offsetWidth - 8;
+    container.innerHTML = '';
+    for (let i = 0; i < side; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < side; j++) {
+            const cell = document.createElement('div');
+            cell.classList.toggle('cell');
+            cell.style.width = `${(width / side)}px`;
+            cell.style.height = `${(width / side)}px`;
+            cell.addEventListener('mouseover', () => {
+                cell.classList.add('hover');
+            });
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
+
+newButton.addEventListener('click', () => {
+    const sideLength = prompt("Enter the new side length (1-100):");
+    if (sideLength < 1 || sideLength > 100 || isNaN(sideLength)) {
+        alert("Please enter a number between 1 and 100.");
+        return;
+    }
+    createGrid(sideLength);
+});
+
+createGrid(16);
